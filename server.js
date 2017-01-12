0a1119c5f2d61eb25ed6f885ea0ebf7ee4a77783
Muriel Silveira <murielkong@gmail.com>
2017-01-12 16:28:43 +0100

    Add the GraphiQL UI and make it work through the proxy

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const RedisStore = require('connect-redis')(session)
 const path = require('path')
 const passport = require('passport')
 const Strategy = require('passport-oauth2').Strategy
+const bodyParser = require('body-parser')
 const config = require('./config')
 
 /*
@@ -36,6 +37,7 @@ function getSessionConfig() {
   return sessionInfo
 }
 
+app.use(bodyParser.json())
 app.use(session(getSessionConfig()))
 app.use(passport.initialize())
 app.use(passport.session())
@@ -72,7 +74,7 @@ app.get('/auth/callback',
   PROXY
 */
 
-app.get('/proxy/graphql', function (req, res) {
+app.post('/proxy/graphql', function (req, res) {
   const passportSession = req.session.passport
   if (!passportSession || !passportSession.user) {
     res.status(401).send('You need to be logged in!')
@@ -86,9 +88,7 @@ app.get('/proxy/graphql', function (req, res) {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${passportSession.user.accessToken}`,
     },
-    body: JSON.stringify({
-      query: config.GRAPHQL.QUERY
-    }),
+    body: JSON.stringify(req.body),
   }).then(function (response) {
     return response.text()
   }).then(function (responseBody) {
@@ -103,6 +103,8 @@ app.get('/proxy/graphql', function (req, res) {
 /*
   INDEX
 */
+
+app.use('/static', express.static('static'))
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
