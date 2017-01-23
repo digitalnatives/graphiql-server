1817161e510256526d30381fddca22f90eece0e9
Muriel Silveira <murielkong@gmail.com>
2017-01-23 17:04:55 +0100

    Add Docker support

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:boron
+
+RUN mkdir -p /usr/src/graphiql-server
+WORKDIR /usr/src/graphiql-server
+
+COPY package.json /usr/src/graphiql-server/
+RUN npm install
+
+COPY . /usr/src/graphiql-server
+
+EXPOSE 5000
--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 const SESSION_SECRET = process.env.GS_SESSION_SECRET || 'some nice key'
 
-const REDIS_SOCKET = process.env.GS_REDIS_SOCKET || process.env.REDISCLOUD_URL || 'redis://localhost:6379'
+const REDIS_SOCKET = (() => {
+  if (process.env.GS_REDIS_TCP_ADDR)
+    return `redis://${process.env.GS_REDIS_TCP_ADDR}:6379`
+
+  if (process.env.GS_REDIS_SOCKET)
+    return process.env.GS_REDIS_SOCKET
+
+  if (process.env.REDISCLOUD_URL)
+    return process.env.REDISCLOUD_URL
+
+  return 'redis://localhost:6379'
+})()
 
 const APP_PORT = process.env.PORT || 5000
 const APP_URL = process.env.GS_APP_URL || `http://localhost:${APP_PORT}`
